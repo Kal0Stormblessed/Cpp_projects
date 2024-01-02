c1d124d9f3a4eb5a510b91330ad64fcef47c6d39
Adi <adriano.muntean@gmail.com>
2024-01-02 03:34:28 +0200

    v2:02.01.2024

--- Leet_twoSum/Leet_twoSum/Leet_twoSum.cpp
+++ Leet_twoSum/Leet_twoSum/Leet_twoSum.cpp
@@ -10,17 +10,21 @@ private:
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
+        // input loop
         std::cout << "Enter the numbers for nums: \n";
         int num;
         while (std::cin >> num && num !='/') {
             nums.push_back(num);
         }
 
+        // setup for error(empty vector)
         if (nums.empty()) {
             std::cerr << "Vector is empty, cannot perform task." << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits <std::streamsize>::max(), '\n');
+            std::cin.clear(); // clear err from cin
+            std::cin.ignore(std::numeric_limits <std::streamsize>::max(), '\n');// ignore all inputs remained in cin
         }
+
+
 
         return std::vector<int>();
     };
