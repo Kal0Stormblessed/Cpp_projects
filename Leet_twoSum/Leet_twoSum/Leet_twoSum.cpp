dda121c3bd0e0f223a9bf280ad75d0dfd49262e7
Adi <adriano.muntean@gmail.com>
2024-01-03 00:32:48 +0200

    v3:03.01.2024

--- Leet_twoSum/Leet_twoSum/Leet_twoSum.cpp
+++ Leet_twoSum/Leet_twoSum/Leet_twoSum.cpp
@@ -10,6 +10,7 @@ private:
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
+        std::vector<int> results;
         // input loop
         std::cout << "Enter the numbers for nums: \n";
         int num;
@@ -24,14 +25,43 @@ public:
             std::cin.ignore(std::numeric_limits <std::streamsize>::max(), '\n');// ignore all inputs remained in cin
         }
 
+        
+        //for loop to check the values in vector for sum = target
+        for (int i = 0; i < nums.size() ; i++) {
+            for (int j = i+1; j < nums.size(); j++) {
 
+                int sumtwo;
+                sumtwo = nums[i] + nums[j];
+                if (sumtwo == target)
+                {
+                  
+                    results.push_back(i);
+                    results.push_back(j);
+                  
+                }
+                    
 
-        return std::vector<int>();
+            }
+        }
+
+        //print result indices
+        for (int index : results) {
+            std::cout << "[" << index << "]" << ", Value:" << nums[index] << std::endl;
+        }
+
+        return results;
     };
 
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution solution;
+    std::vector<int> nums;
+
+    int target;
+    std::cout << "Enter target value: ";
+    std::cin >> target;
+
+    std::vector<int> result = solution.twoSum(nums, target);
 }
