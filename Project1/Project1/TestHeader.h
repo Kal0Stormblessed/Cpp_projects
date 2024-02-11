3567c5178d6091be8f3754aee0a45242fcecf122
Adi <adriano.muntean@gmail.com>
2024-02-11 14:05:37 +0200

    v02:11.02.2024:Made changes to the color function in the test header, added backgounrd coloring and simplified by removing switch case.

--- Project1/Project1/TestHeader.h
+++ Project1/Project1/TestHeader.h
@@ -11,80 +11,76 @@ namespace coloring {
 		std::cout << "\033[31;107;1;3mHello\033[0m" << std::endl;
 
 	}
-	int inputHandler (std::string inputColor)
+	int frinputHandler (std::string frinputColor)
 	{
 		int number {};
-
+		
 		//supported colors: Black, Red, Green, Yellow, Blue, Magenta, Cyan, GrayWhite, GrayBlack.
-		//supported colorc: 1    , 2  , 3    , 4     , 5   , 6      , 7   , 8        , 9
-		std::vector<std::string> colorList = { "Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "GrayWhite", "GrayBlack" };
+		std::vector<std::string> colorList = { "Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "GrayWhite" };
 
 		std::unordered_map<std::string, int> supColors;
-		int counter = 1;
+		int counter = 30;
 		for (const auto& color : colorList)
 		{
 			supColors.emplace(color, counter);
 			counter++;
 		}
 
-		auto aux = supColors.find(inputColor);
+		auto aux = supColors.find(frinputColor);
 		if (aux != supColors.end()) {
 			number = aux->second;
 		}
 		else {
-			std::cerr << "Color not found: " << inputColor << std::endl;
+			std::cerr << "Color not found: " << frinputColor << std::endl;
 
 			std::cout << "Enter valid color: ";
-			std::cin >> inputColor;
+			std::cin >> frinputColor;
 		}
-		std::cout << number<<std :: endl;
+		//std::cout << number<<std :: endl;
+		return number;
+	}
+	int bginputHandler(std::string bginputColor)
+	{
+		int number{};
+
+		std::vector<std::string> colorList = { "Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "GrayWhite" };
+
+		std::unordered_map<std::string, int> supColors;
+		int counter = 40;
+		for (const auto& color : colorList)
+		{
+			supColors.emplace(color, counter);
+			counter++;
+		}
+
+		auto aux = supColors.find(bginputColor);
+		if (aux != supColors.end()) {
+			number = aux->second;
+		}
+		else {
+			std::cerr << "Color not found: " << bginputColor << std::endl;
+
+			std::cout << "Enter valid color: ";
+			std::cin >> bginputColor;
+		}
+		//std::cout << number << std::endl;
 		return number;
 	}
 	void colorText(std::string text)
 	{
-		int cases;
-		std::string inputColor;
+		int frCases{}, bgCases;
+		std::string frinputColor, bginputColor;
 
-		std::cout << "Specify color: ";
-		std::cin >> inputColor;
+		std::cout << "Specify foreground color: ";
+		std::cin >> frinputColor;
+		
+		std::cout << "Specify background color: ";
+		std::cin >> bginputColor;
 
-		cases = inputHandler(inputColor);
+		frCases = frinputHandler(frinputColor);
+		bgCases = bginputHandler(bginputColor);
 
-		switch (cases)
-		{
-			case 1:
-				std::cout << "\033[30;40m"<< text <<"\033[0m" << std::endl;
-				break;
-
-			case 2:
-				std::cout << "\033[31;40m" << text << "\033[0m" << std::endl;
-				break;
-
-			case 3:
-				std::cout << "\033[32;40m" << text << "\033[0m" << std::endl;
-				break;
-
-			case 4:
-				std::cout << "\033[33;40m" << text << "\033[0m" << std::endl;
-				break;
-			case 5:
-				std::cout << "\033[34;40m" << text << "\033[0m" << std::endl;
-				break;
-			case 6:
-				std::cout << "\033[35;40m" << text << "\033[0m" << std::endl;
-				break;
-			case 7:
-				std::cout << "\033[36;40m" << text << "\033[0m" << std::endl;
-				break;
-			case 8:
-				std::cout << "\033[37;40m" << text << "\033[0m" << std::endl;
-				break;
-			case 9:
-				std::cout << "\033[90;40m" << text << "\033[0m" << std::endl;
-				break;
-			
-		}
-					 
+		std::cout << "\033["<< frCases <<";"<< bgCases << "m" << text <<"\033[0m" << std::endl;	 
 	}
 }
 #endif
