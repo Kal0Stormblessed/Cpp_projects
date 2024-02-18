36f067001f8a4f49ffd061b7f4f8711604040751
Adi <adriano.muntean@gmail.com>
2024-02-18 22:42:40 +0200

    Completed most of code, need to add implementation for Tetrominoes

--- Tetris_raylib/Tetris_raylib/main.cpp
+++ Tetris_raylib/Tetris_raylib/main.cpp
@@ -1,22 +1,20 @@
-#include "raylib.h"
 #include "Game.h"
+#include "Settings.h"
  
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    Game game{ screenWidth, screenHeight, 60 , "Tetris raylib" };
+    
+    Game game{ settings::screenWidth,
+                settings::screenHeight,
+                settings::fps,
+                "Tetris Raylib"};
 
     while (!game.GameShouldClose())
     {
         game.Tick();
-    }
-    
+    }   
     return 0;
 }
